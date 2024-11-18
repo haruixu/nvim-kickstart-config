eb5d70656e77eaf1f5d7bf6130bdf7c6aaa348cc
haruixu <xulr0820@hotmail.com>
2024-11-18 14:16:38 +0100

    Disallow harpoon lazy load

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,15 +1,7 @@
 return {
   {
     'ThePrimeagen/harpoon', -- Plugin name
-    lazy = true, -- Load the plugin lazily
-    keys = { -- Keys that will trigger loading the plugin
-      { '<leader>a', desc = 'Add file to Harpoon' },
-      { '<leader>e', desc = 'Toggle Harpoon menu' },
-      { '<C-h>', desc = 'Navigate to Harpoon file 1' },
-      { '<C-j>', desc = 'Navigate to Harpoon file 2' },
-      { '<C-k>', desc = 'Navigate to Harpoon file 3' },
-      { '<C-l>', desc = 'Navigate to Harpoon file 4' },
-    },
+    lazy = false, -- Load the plugin lazily
     config = function()
       -- Require harpoon modules
       local mark = require 'harpoon.mark'
@@ -33,11 +25,25 @@ return {
         ui.nav_file(4)
       end, { desc = 'Navigate to Harpoon file 4' })
 
+      require('harpoon').setup {
+        -- Override the tabline setting
+        tabline = true, -- Enable the tabline
+        tabline_prefix = '   ', -- Example of a custom prefix
+        tabline_suffix = '   ', -- Example of a custom suffix
+      }
+
       vim.cmd 'highlight! HarpoonInactive guibg=NONE guifg=#63698c'
       vim.cmd 'highlight! HarpoonActive guibg=NONE guifg=white'
       vim.cmd 'highlight! HarpoonNumberActive guibg=NONE guifg=#7aa2f7'
       vim.cmd 'highlight! HarpoonNumberInactive guibg=NONE guifg=#7aa2f7'
       vim.cmd 'highlight! TabLineFill guibg=NONE guifg=white'
+
+      -- Make harpoon menu widht relative to screen width
+      -- require('harpoon').setup {
+      --   menu = {
+      --     width = vim.api.nvim_win_get_width(0) - 100,
+      --   },
+      -- }
     end,
   },
 }
